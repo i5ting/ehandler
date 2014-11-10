7f03d798cc5fab3b31efd5acc44f3dd0ecc7d17e
alfred sang <i5ting@126.com>
2014-11-10 10:59:18 +0800

    git remote add origin git@github.com:i5ting/ehandler.git

---
+++ ehandler/ehandler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ehandler
+//
+//  Created by sangalfred on 11/10/14.
+//  Copyright (c) 2014 sangalfred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ehandler/ehandler/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  ehandler
+//
+//  Created by sangalfred on 11/10/14.
+//  Copyright (c) 2014 sangalfred. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (IBAction)dsdsds:(id)sender {
+    [self performSelector:@selector(aaa) withObject:nil];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self performSelector:@selector(aaa) withObject:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
